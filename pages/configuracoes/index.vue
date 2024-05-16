e9605ccd1485a89595ac7c26c405e3b41501bcce
Samuel Silva <samuel7silva@outlook.com>
2024-05-16 16:00:28 -0300

    Ajusta o layout da tela de configuracoes

--- pages/configuracoes/index.vue
+++ pages/configuracoes/index.vue
@@ -65,10 +65,6 @@
                 <UInput v-model="state.NAME" />
               </UFormGroup>
 
-              <UFormGroup label="Apelido" name="NICK_NAME">
-                <UInput v-model="state.NICK_NAME" />
-              </UFormGroup>
-
               <UFormGroup
                 v-mask="'##.###.###/####-##'"
                 label="CNPJ"
@@ -87,10 +83,31 @@
                   value-attribute="value"
                 />
               </UFormGroup>
+              <h5 class="py-3.5 border-b border-gray-200 dark:border-gray-700">
+                Estilo
+              </h5>
+              <UFormGroup label="Cor principal" name="PRIMARY_COLOR">
+                <div class="flex flex-col gap-2 w-1/2">
+                  <div
+                    class="h-8 w-100% rounded-md overflow-hidden shadow-sm"
+                    :style="{
+                      'background-color': `${state.PRIMARY_COLOR}`,
+                    }"
+                  >
+                    <ColorPicker
+                      v-model:pure-color="state.PRIMARY_COLOR"
+                      picker-type="chrome"
+                      format="hex"
+                      style="width: 100% !important"
+                    />
+                  </div>
+                  <!-- <UInput v-model="state.PRIMARY_COLOR" /> -->
+                </div>
+              </UFormGroup>
             </div>
-            <div class="flex flex-col p-4 col-span-2">
+            <div class="flex flex-col gap-3 p-4 col-span-2">
               <div
-                class="bg-gray-200 border h-64 mb-4 mt-2 flex items-center justify-center rounded-md text-gray-500"
+                class="bg-gray-200 border h-64 flex items-center justify-center rounded-md text-gray-500"
                 :style="{
                   'background-image': `url(${state.LOGO_IMAGE})`,
                   'background-position': 'center',
@@ -102,10 +119,13 @@
               </div>
               <UFormGroup label="Logotipo (Url da imagem)" name="LOGO_IMAGE">
                 <div class="flex gap-2 items-center justify-center">
-                  <UInput v-model="state.LOGO_IMAGE" class="flex-grow" />
+                  <UInput
+                    v-model="state.LOGO_IMAGE"
+                    class="flex-grow relative"
+                  />
                   <div
                     v-if="state.LOGO_IMAGE"
-                    class="flex text-red-600 items-center justify-center rounded-sm cursor-pointer hover:text-red-500 transition-opacity"
+                    class="flex text-red-600 items-center bg-white p-1 h-full justify-center rounded-r-md cursor-pointer absolute top-0 right-0 hover:text-red-500 transition-opacity"
                     title="Remover imagem"
                     @click="state.LOGO_IMAGE = undefined"
                   >
@@ -116,29 +136,11 @@
             </div>
           </div>
 
-          <h5 class="py-3.5 border-b border-gray-200 dark:border-gray-700">
+          <!-- <h5 class="py-3.5 border-b border-gray-200 dark:border-gray-700">
             Estilo
-          </h5>
-          <UFormGroup label="Cor principal" name="PRIMARY_COLOR">
-            <div class="flex flex-col gap-2 w-1/4">
-              <div
-                class="h-8 w-100% rounded-md overflow-hidden"
-                :style="{
-                  'background-color': `${state.PRIMARY_COLOR}`,
-                }"
-              >
-                <ColorPicker
-                  v-model:pure-color="state.PRIMARY_COLOR"
-                  picker-type="chrome"
-                  format="hex"
-                  style="width: 100% !important"
-                />
-              </div>
-              <UInput v-model="state.PRIMARY_COLOR" />
-            </div>
-          </UFormGroup>
+          </h5> -->
 
-          <h5 class="py-3.5 border-b border-gray-200 dark:border-gray-700 mt-3">
+          <!-- <h5 class="py-3.5 border-b border-gray-200 dark:border-gray-700 mt-3">
             SMTP (Configurações de envio de e-mail)
           </h5>
           <div class="py-4 flex gap-4 flex-col col-span-3">
@@ -154,7 +156,7 @@
             <UFormGroup label="HOST_FROM_NAME" name="PRIMARY_COLOR">
               <UInput v-model="state.HOST_FROM_NAME" class="flex-grow" />
             </UFormGroup>
-          </div>
+          </div> -->
         </div>
       </UForm>
     </UCard>
@@ -196,7 +198,6 @@ const closeModal = () => {
 const schema = object({
   TYPE: number().required("Selecione um tipo").typeError("Valor inválido"),
   NAME: string().required("O nome deve ser preenchido"),
-  NICK_NAME: string(),
   CNPJ: string().test("test-invalid-cnpj", "CNPJ inválido", (cnpj) => {
     if (cnpj) {
       return config.validateCNPJ(cnpj);
@@ -209,17 +210,15 @@ type Schema = InferType<typeof schema>;
 
 const state = reactive({
   NAME: undefined,
-  NICK_NAME: undefined,
   CNPJ: undefined,
   TYPE: undefined,
 
   LOGO_IMAGE: undefined,
   PRIMARY_COLOR: undefined,
-
-  HOST: undefined,
-  HOST_PASSWORD: undefined,
-  HOST_FROM: undefined,
-  HOST_FROM_NAME: undefined,
+  // HOST: undefined,
+  // HOST_PASSWORD: undefined,
+  // HOST_FROM: undefined,
+  // HOST_FROM_NAME: undefined,
 
   NRORG: NrOrg,
   MODIFIED_BY: UserId,
@@ -236,17 +235,15 @@ onMounted(async () => {
     if (response) {
       loading.value = false;
       state.NAME = response.NAME;
-      state.CNPJ = response.CNPJ;
+      state.CNPJ = response.CNPJ === null ? "" : response.CNPJ;
       state.TYPE = response.TYPE;
-      state.NICK_NAME = response.NICK_NAME;
 
       state.LOGO_IMAGE = response.gen_configuration.LOGO_IMAGE;
       state.PRIMARY_COLOR = response.gen_configuration.PRIMARY_COLOR;
-
-      state.HOST = response.gen_configuration.HOST;
-      state.HOST_PASSWORD = response.gen_configuration.HOST_PASSWORD;
-      state.HOST_FROM = response.gen_configuration.HOST_FROM;
-      state.HOST_FROM_NAME = response.gen_configuration.HOST_FROM_NAME;
+      // state.HOST = response.gen_configuration.HOST;
+      // state.HOST_PASSWORD = response.gen_configuration.HOST_PASSWORD;
+      // state.HOST_FROM = response.gen_configuration.HOST_FROM;
+      // state.HOST_FROM_NAME = response.gen_configuration.HOST_FROM_NAME;
     }
   } catch (error) {
     loading.value = false;
@@ -284,22 +281,49 @@ options.value = [
     value: 7,
     label: "Igrejas",
   },
+  {
+    value: 8,
+    label: "Grupos",
+  },
+  {
+    value: 9,
+    label: "Condomínios",
+  },
+  {
+    value: 10,
+    label: "Família",
+  },
+  {
+    value: 11,
+    label: "Partido político",
+  },
+  {
+    value: 12,
+    label: "Resorts",
+  },
+  {
+    value: 13,
+    label: "Colégios",
+  },
+  {
+    value: 14,
+    label: "Times",
+  },
 ];
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   showError.value = false;
   const data = {
     NAME: state.NAME,
-    CNPJ: config.cleanCNPJ(state.CNPJ),
+    CNPJ: state.CNPJ !== "" ? config.cleanCNPJ(state.CNPJ) : null,
     TYPE: Number(event.data.TYPE),
-    NICK_NAME: state.NICK_NAME,
     LOGO_IMAGE: state.LOGO_IMAGE,
     LOGO_IMAGE_SMALL: state.LOGO_IMAGE,
     PRIMARY_COLOR: state.PRIMARY_COLOR,
-    HOST: state.HOST,
-    HOST_PASSWORD: state.HOST_PASSWORD,
-    HOST_FROM: state.HOST_FROM,
-    HOST_FROM_NAME: state.HOST_FROM_NAME,
+    // HOST: state.HOST,
+    // HOST_PASSWORD: state.HOST_PASSWORD,
+    // HOST_FROM: state.HOST_FROM,
+    // HOST_FROM_NAME: state.HOST_FROM_NAME,
   };
   isLoading.value = true;
 
